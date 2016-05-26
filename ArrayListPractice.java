fbc5e5925f1f640dab3a578c7487977aea69835a
Dmytro Stashok <dmytro_stashok@epam.com>
2016-05-26 11:42:15 +0300

    elements printed in backward ordred

--- ArrayListPractice.java
+++ ArrayListPractice.java
@@ -1,11 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ArrayListPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int[] one = new int[5];
         System.out.println(one.length);
         //Create an ArrayList - it is dynamic
-        ArrayList <String> two = new ArrayList<>();
+        ArrayList<String> two = new ArrayList<>();
         System.out.println(two.size());
         //Set values to ArrayList
         two.add("Everybody ");
@@ -13,7 +16,7 @@ public class ArrayListPractice {
         two.add("now");
         System.out.println(two.size());
         //Print all elements in ArrayList
-        for(String i:two){
+        for (String i : two) {
             System.out.println(i);
         }
 
@@ -24,9 +27,9 @@ public class ArrayListPractice {
         }
         System.out.println(" ");
         //Rename value in ArrayList
-        two.set(0,"C'mon, everybody");
+        two.set(0, "C'mon, everybody");
 ///some changes to test GitHub
-        for(String i:two){
+        for (String i : two) {
             System.out.println(i);
         }
         System.out.println(" ");
@@ -35,8 +38,20 @@ public class ArrayListPractice {
         System.out.println(two.size());
         System.out.println(" ");
 
-        for(String i:two){
+        for (String i : two) {
             System.out.println(i);
+        }
+        ///add few elements and print in backward order
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String s = reader.readLine();
+            list.add(s);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(4 - i));
         }
 
     }
